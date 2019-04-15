1c261793ea198713b0533713892808367e967efc
Ónodi Zsolt <onodi.zs@gmail.com>
2019-04-15 22:39:27 +0200

    Added the popup window functionality

--- js/map.js
+++ js/map.js
@@ -53,10 +53,69 @@ $(document).ready(function(){
         zoom: 5
     });
     
+    var container = $("#popup")[0];
+    
+    var overlay = new ol.Overlay({
+        element : container,
+        autoPan: true,
+        autoPanAnimation: {
+          duration: 250
+        }        
+    });    
+    
     var map = new ol.Map({
         view : view,
         layers : [osm, routesLayer, stopsLayer],
         target : 'map',
-        controls : [zoomButton, scaleBar]
+        controls : [zoomButton, scaleBar],
+        overlays : [overlay]
     });
+    
+    map.on('singleclick', function(evt) {
+        var popupdrawn = false;
+        map.forEachFeatureAtPixel(evt.pixel, function(feature, layer) {
+            if(layer.getProperties().title == 'Állomások' && !popupdrawn){
+                buildStopsPopupHtmlContent(feature);
+                popupdrawn = true;
+            }else if(layer.getProperties().title == 'Útvonal' && !popupdrawn){
+                buildRoutesPopupHtmlContent(feature, evt.coordinate);
+                popupdrawn = true;
+            }
+        }, {hitTolerance : 2});
+    });
+    
+    $("#popup-closer").click(function() {
+        overlay.setPosition(undefined);
+        // $(this).blur();
+        // return false;
+    });
+    
+    function buildStopsPopupHtmlContent(feature){
+        var coordinate = feature.getGeometry().getCoordinates();
+        var props = feature.getProperties();
+        
+        var contentHtml = '<div class = "popupHtml">' +
+                      props.day + '. nap ' + props.stop + '. állomás: ' + '<br/>' + 
+                      props.place + '<br/>' +
+                      '<img class = "popupImage" src = "img/nordtrip/' + props.pic + '"/><br/>' +
+                      props.desc + '<br/>' +
+                      '</div>';
+        
+        overlay.setPosition(coordinate);
+        
+        $("#popup-content").html(contentHtml);
+    }
+    
+    function buildRoutesPopupHtmlContent(feature, clickedpoint){
+        var coordinate = feature.getGeometry().getClosestPoint(clickedpoint);
+        var props = feature.getProperties();
+        
+        var contentHtml = '<div class = "popupHtml">' +
+                      '<a class = "popupLink" href = "' + props.blog + '" target = "blank">NordTrip ' + props.day + '. nap (' + props.date + ')</a>' + 
+                      '</div>';
+        
+        overlay.setPosition(coordinate);
+        
+        $("#popup-content").html(contentHtml);        
+    }
 });
